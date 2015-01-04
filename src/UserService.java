9855c1768f3379aa6022d802ee683d989b001e7c
sambagurd <sambagurd@gmail.com>
2015-01-04 16:56:16 -0600

    First Commit

---
+++ src/UserService.java
@@ -0,0 +1,63 @@
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+ 
+@Path("/users")
+public class UserService {
+ 
+	//@QueryParam example
+	
+	/*
+	@GET
+	@Path("/query")
+	public Response getUsers(
+		@QueryParam("from") int from,
+		@QueryParam("to") int to,
+		@QueryParam("orderBy") List<String> orderBy) {
+ 
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+ 
+	}*/
+	
+	//Programmatic Query Parameter
+	/*
+	@GET
+	@Path("/query")
+	public Response getUsers(@Context UriInfo info) {
+ 
+		String from = info.getQueryParameters().getFirst("from");
+		String to = info.getQueryParameters().getFirst("to");
+		List<String> orderBy = info.getQueryParameters().get("orderBy");
+ 
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+ 
+	}*/
+	
+	//@DefaultValue example
+	@GET
+	@Path("/query")
+	public Response getUsers(
+		@DefaultValue("1000") @QueryParam("from") int from,
+		@DefaultValue("999")@QueryParam("to") int to,
+		@DefaultValue("name") @QueryParam("orderBy") List<String> orderBy) {
+ 
+		return Response
+		   .status(200)
+		   .entity("getUsers is called, from : " + from + ", to : " + to
+			+ ", orderBy" + orderBy.toString()).build();
+ 
+	}
+ 
+}
